93f0cb85196cab9738cd53b04584e20494714f3c
rituparna-india1947 <85564017+rituparna-india1947@users.noreply.github.com>
2022-03-07 12:55:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+	img = loadImage("mario05.png");
+	mario_jump = loadSound("jump.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_coin = loadSound("coin.wav");
+}
+function setup() {
+	canvas = createCanvas(1240, 336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent("game_console");
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+}
+
+function gotPoses(results) {
+	if (results.length > 0) {
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
+
+function draw() {
+	game();
+}
